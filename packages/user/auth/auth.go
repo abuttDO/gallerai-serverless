b57940675bb83400e74d523e26bc65cadbb50650
Kyle Redelinghuys <kyle@ksred.com>
2022-08-09 13:42:29 +0100

    Restructured application

--- packages/user/auth/auth.go
+++ packages/user/auth/auth.go
@@ -5,15 +5,15 @@ import (
 	"github.com/google/uuid"
 )
 
-// Request is for user log in
+// Request is for auth log in
 type Request struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
-// Response returns back the http code, type of data, and the presigned url to the user.
+// Response returns back the http code, type of data, and the presigned url to the auth.
 type Response struct {
-	// StatusCode is the http code that will be returned back to the user.
+	// StatusCode is the http code that will be returned back to the auth.
 	StatusCode int `json:"statusCode,omitempty"`
 	// Headers is the information about the type of data being returned back.
 	Headers map[string]string `json:"headers,omitempty"`
